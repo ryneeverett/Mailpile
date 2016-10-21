d6106367e002e05f8220a356e89b987017a6aa42
ryneeverett <ryneeverett@gmail.com>
2016-10-20 23:12:39 -0400

    Fix docker setup.

--- mp.sh
+++ mp.sh
@@ -1,4 +1,14 @@
+_mp() {
+    docker-compose -f .docker-compose.yml run --rm --service-ports mailpile "$@"
+}
+
+mp-setup() {
+    docker volume rm mailpile_data
+    _mp setup
+    _mp --www=0.0.0.0:33411
+}
+
 mp() {
     echo "http://127.0.0.1:33411"
-    docker-compose -f .docker-compose.yml run --rm --service-ports mailpile
+    _mp "$@"
 }
